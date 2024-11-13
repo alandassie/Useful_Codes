8f3614bc59612f86d80e48018e0abde4ff0f4fe6
Alan Dassie <alan.dassie@ganil.fr>
2024-11-13 18:45:28 +0100

    Preparing the code

--- GSMCC-CODE/DATA_ANALYSIS/CalcPartialWidth.py
+++ GSMCC-CODE/DATA_ANALYSIS/CalcPartialWidth.py
@@ -1,36 +1,88 @@
 """
     Created on June 2024 by Alan D.K.
 
-    This code sum up the width from a GSMCC output in order to distinguish 
-    the mass partition which is more important.
+    This is a multitask code used for the analysis of the GSMCC outputs.
+    The analysis done by the code are the followings:
+    + sum up the partial widths for each mass partition 
+    and shown those greater than 5% of the total.
+    + sum up the occ prob for each mass partition 
+    and shown those greater than 5% of the total.
     
-    The name of the input file must be defined in the file "Data_Analysis.in"
+    The name of the file to be analyzed must be defined in the 
+    file "Data_Analysis.in". An example of the input file is at the end of the code.
 """
 
-from datetime import datetime
-
-# Output file name
-# now = datetime.now()
-# optout = 'Sum%s.out'% (now.strftime("%y.%m.%d_%H:%M"))
-# # Erease output file
-# with open(optout,'w') as output:
-#     pass
-
-# Declaration of funcitons
-def print_twice(*args,**kwargs): # Allow to print in file and in terminal at the same line
-    print(*args,**kwargs)
-    with open(optout,"a") as f:  # appends to file and closes it when finished
-        print(file=f,*args,**kwargs)
-        
 # Input file
 inputfile = 'Data_Analysis.in' 
 with open(inputfile, 'r') as readfile:
     dataaux = readfile.read().split('\n')
-namefile = dataaux[0]
-# Reading from the file
+namefile = dataaux[1]
+jpi_state = dataaux[3]
+index_state = dataaux[5]
+# Output file
+jpi_state_write = jpi_state.replace('/','I')
+outputfile = namefile[:-4] + '.DataAnalysis-' + jpi_state_write + '_' + index_state
+#
+# Declaration of funcitons
+# def erease_output_file():
+#     with open(logfile,'w') as output:
+#         pass
+def print_twice(*args,**kwargs): # Allow to print in file and in terminal at the same line
+    print(*args,**kwargs)
+    with open(outputfile,"a") as f:  # appends to file and closes it when finished
+        print(file=f,*args,**kwargs)
+# .-
+# Idea from https://stackoverflow.com/questions/3961265/get-line-number-of-certain-phrase-in-text-file
+def searchline(file,phrase):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        if phrase in text:
+            phrase_index = text.index(phrase)
+            l_num = text[:phrase_index].count('\n')  # Nth line has n-1 '\n's before
+        else:
+            l_num = None
+    return l_num
+# Idea from https://stackoverflow.com/questions/3873361/finding-multiple-occurrences-of-a-string-within-a-string-in-python
+def searchline_all(file,phrase):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        count = 0
+        i = len(phrase)
+        x = 0
+        l_num = []
+        while x < len(text) - (i-1):
+            if text[x:x+i] == phrase:
+                l_num.append(text[:x].count('\n'))  # Nth line has n-1 '\n's before
+                x += i
+                count += 1
+            else:
+                x += 1
+    return l_num
+#
+def searchlinefinal(file,phrase):
+    with open(file, 'r', encoding='utf-8') as f:
+        text = f.read()
+        if phrase in text:
+            phrase_index = text.rindex(phrase)
+            l_num = text[:phrase_index].count('\n')  # Nth line has n-1 '\n's before
+        else:
+            l_num = None
+    return l_num
+# .-
+#
+# Defining the lines of data
+line = searchline(namefile,"%s (%s) resonant state"% (jpi_state,index_state))
+
+# Reading data
 with open(readfilename, 'r') as readfile:
     data = readfile.read().split('\n')
     #
+    
+    
+    
+    
+    
+    
 # Build two arrays
 projectile_width = []
 current_width = []
@@ -88,13 +140,15 @@ for i in range(0,len(projectile_width)):
     print('\n')
 
 
-
-
-
-
 """
-    This program is free software: you can redistribute it and/or 
-    modify it under the terms of the GNU General Public License as 
-    published by the Free Software Foundation, either version 3 of 
-    the License, or (at your option) any later version.
+    Example of GSMCC_DCS_ene-ang.in file:
+    _________________________________
+    GSMCC-FILE
+    IN2P3_11C_CC_GSMOpt-24.08.26-11.00_Basis-24.10.30-15.00_ICF-24.11.04-16.30_SF-24.11.13-13.00-3I2-.out
+    JPi-STATE
+    3/2-
+    INDEX
+    3
+    _________________________________
+    
 """
